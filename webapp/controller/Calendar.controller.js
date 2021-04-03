f446e5f1220dcbd3a8826256042dedbd7cf24229
Vsevolod <shaldin2000@mail.ru>
2021-04-03 20:35:25 +0000

    step 3: upd

--- webapp/controller/Calendar.controller.js
+++ webapp/controller/Calendar.controller.js
@@ -13,6 +13,9 @@ sap.ui.define([
 
 	    return Controller.extend("itmo2021calendarsvv.controller.Calendar", {
             _holidays: {},
+            _nonWorkingId: 0,
+            _workingId: 0,
+            _partlyId: 0,
             _currentYear: "2021",
 
             onInit: function() {
@@ -28,15 +31,35 @@ sap.ui.define([
                 this._getDays(oDays, oCalendar);
             },
 
+            _addHoliday: function(oLegend, oId, oTitle) {
+                oLegend.addItem(new sap.ui.unified.CalendarLegendItem({
+                    text: oTitle
+                }));
+                this._holidays[oId] = oTitle;
+            },
+
             _getHolidays: function(oHolidays, oLegend) {
+                var nId = 1;
                 for (var holiday of oHolidays.children) {
                     var oAttributes = holiday.attributes;
                     var oId = oAttributes.id.nodeValue;
                     var oTitle = oAttributes.title.nodeValue;
-                    oLegend.addItem(new sap.ui.unified.CalendarLegendItem({
-						text: oTitle
-                    }));
-                    this._holidays[oId] = oTitle;
+                    this._addHoliday(oLegend, oId, oTitle);
+                    nId++;
+                }
+                this._nonWorkingId = nId;
+                this._addHoliday(oLegend, nId++, "Дополнительный выходной");
+                this._workingId = nId;
+                this._addHoliday(oLegend, nId++, "Дополнительный рабочий день");
+                this._partlyId = nId;
+                this._addHoliday(oLegend, nId, "Сокращенный рабочий день");
+            },
+
+            _getType: function(oNumber) {
+                if (oNumber < 10) {
+                    return "Type0" + oNumber;
+                } else {
+                    return "Type" + oNumber;
                 }
             },
 
@@ -49,17 +72,23 @@ sap.ui.define([
                     var oFrom = oAttributes.f !== undefined ? oAttributes.f.nodeValue : null;
                     var sType = "";
                     
-                    if (oType === "1") {
-                        sType = sap.ui.unified.CalendarDayType.NonWorking;
-                    } else {
-                        sType = sap.ui.unified.CalendarDayType.None;
-                    }
-                    if (oHoliday !== null) {
-                        if (oHoliday < 10) {
-                            sType = "Type0" + oHoliday;
-                        } else {
-                            sType = "Type" + oHoliday;
-                        }
+                    switch (oType) {
+                        case "1":
+                            if (oHoliday !== null) {
+                                sType = this._getType(oHoliday);
+                            } else {
+                                sType = this._getType(this._nonWorkingId);
+                                oHoliday = this._nonWorkingId;
+                            }
+                            break;
+                        case "2":
+                            sType = this._getType(this._partlyId);
+                            oHoliday = this._partlyId;
+                            break;
+                        case "3":
+                            sType = this._getType(this._workingId);
+                            oHoliday = this._workingId;
+                            break;
                     }
 
                     var oDate = new Date(this._currentYear, oDay.split('.')[0] - 1, oDay.split('.')[1]);
